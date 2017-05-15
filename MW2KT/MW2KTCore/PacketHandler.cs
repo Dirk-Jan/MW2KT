75462e507fa5bd8abf67e6fb145e14c2fbade7ee
Dirk-Jan Beijer <djjadebeijer@gmail.com>
2017-05-15 15:36:40 +0200

    Update

---
+++ MW2KT/MW2KTCore/PacketHandler.cs
@@ -0,0 +1,17 @@
+﻿using PcapDotNet.Packets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MW2KTCore
+{
+    public static class PacketHandler
+    {
+        public static void HandlePacket(Packet packet)
+        {
+
+        }
+    }
+}
---
+++ MW2KT/Test Pcap/Program.cs
@@ -0,0 +1,65 @@
+﻿using PcapDotNet.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Pcap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Retrieve the interfaces list
+            IList<LivePacketDevice> allDevices = LivePacketDevice.AllLocalMachine;
+
+            // Scan the list printing every entry
+            for (int i = 0; i != allDevices.Count(); ++i)
+                DevicePrint(allDevices[i]);
+
+            Console.ReadLine();
+        }
+
+        // Print all the available information on the given interface
+        private static void DevicePrint(IPacketDevice device)
+        {
+            bool containsIPV4Address = false;
+            foreach (DeviceAddress address in device.Addresses)
+                if (address.Address.Family == SocketAddressFamily.Internet)
+                    containsIPV4Address = true;
+            if (!containsIPV4Address)
+                return;
+
+
+            // Name
+            Console.WriteLine(device.Name);
+
+            // Description
+            if (device.Description != null)
+                Console.WriteLine("\tDescription: " + device.Description);
+
+            // Loopback Address
+            Console.WriteLine("\tLoopback: " +
+                              (((device.Attributes & DeviceAttributes.Loopback) == DeviceAttributes.Loopback)
+                                   ? "yes"
+                                   : "no"));
+
+            // IP addresses
+            foreach (DeviceAddress address in device.Addresses)
+            {
+                Console.WriteLine("\tAddress Family: " + address.Address.Family);
+
+                if (address.Address != null)
+                    Console.WriteLine(("\tAddress: " + address.Address));
+                if (address.Netmask != null)
+                    Console.WriteLine(("\tNetmask: " + address.Netmask));
+                if (address.Broadcast != null)
+                    Console.WriteLine(("\tBroadcast Address: " + address.Broadcast));
+                if (address.Destination != null)
+                    Console.WriteLine(("\tDestination Address: " + address.Destination));
+            }
+            Console.WriteLine();
+        }
+    }
+}
